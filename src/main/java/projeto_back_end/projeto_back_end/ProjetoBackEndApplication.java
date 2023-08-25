74cfbef4218ae899eb883a1322b6fdc9b5272134
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2023-08-25 18:21:31 -0500

    initial project

---
+++ src/main/java/projeto_back_end/projeto_back_end/ProjetoBackEndApplication.java
@@ -0,0 +1,13 @@
+package projeto_back_end.projeto_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto_back_end/projeto_back_end/ProjetoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_back_end.projeto_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
